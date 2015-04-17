946133285172f208fadd5cb7783a01d7fd9c7064
Kevin R. Zhang <kevin.zhangrui@gmail.com>
2015-04-17 13:34:28 -0700

    added flapper news

---
+++ flapper-news/app/assets/javascripts/app.js
@@ -0,0 +1,21 @@
+var app = angular.module('flapperNews', ['ui.router', 'templates']);
+
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: 'home/_home.html',
+      controller: 'MainCtrl'
+    })
+    .state('posts', {
+	  url: '/posts/{id}',
+	  templateUrl: 'posts/_posts.html',
+	  controller: 'PostsCtrl'
+	});
+
+  $urlRouterProvider.otherwise('home');
+}]);
